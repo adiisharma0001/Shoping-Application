6d41170297e08cde862846cb024a6c64ef9dcd68
Aditya <95631458+Sharmaaditya05@users.noreply.github.com>
2023-04-19 23:01:20 +0530

    ....

--- index.js
+++ index.js
@@ -28,7 +28,10 @@ function getProductById(id) {
   const products = [
     { id: '1', name: 'Product 1', price: 19999 },
     { id: '2', name: 'Product 2', price: 24999 },
-    { id: '3', name: 'Product 3', price: 149999 }
+    { id: '3', name: 'Product 3', price: 149999 },
+    { id: '4', name: 'Product 4', price: 14999 },
+    { id: '5', name: 'Product 5', price: 9999 },
+    { id: '6', name: 'Product 6', price: 49999 }
   ];
   return products.find(product => product.id === id);
 }
